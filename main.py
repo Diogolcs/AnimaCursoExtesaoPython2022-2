dd53882a2fb604495b939b4e2e4bc012a6b369a8
Diogo Lacerda <116228981+Diogolcs@users.noreply.github.com>
2022-11-10 00:00:21 +0000

    .

--- main.py
+++ main.py
@@ -56,20 +56,72 @@
 
 
 #Exemplo de laço
-print("Maneira rústica")
-print("1")
-print("2")
-print("3")
-print("4")
-print("5")
-print("6")
-print("7")
-print("8")
-print("9")
-print("10\n")
+# print("Maneira rústica")
+#  print("1")
+# print("2")
+# print("3")
+# print("4")
+# print("5")
+# print("6")
+# print("7")
+# print("8")
+# print("9")
+# print("10\n")
 
 
-numero = 1
-print(numero)
-numero = numero + 1
-print (numero)
+# numero = 1
+# print(numero)
+# numero = numero + 1
+# print (numero)
+
+print("Aula 3 - 09/11/22\n")
+
+#Exibir de 1 a a 10
+contador = 1
+
+#while(contador <=10):
+#  print(contador)
+#  contador = contador+1
+
+#Laço for
+
+fruta = "morango"
+print(fruta)
+
+fruta1 = "morango"
+fruta2 = "laranja"
+fruta3 = "pêra"
+
+#Lista
+
+frutas = ["morango", "laranja", "pêra"]
+
+#quero exibir apenas a 3 fruta(0,1,2)
+
+print(frutas)
+
+print(frutas[2])
+
+#exibir quantas frutas tem na lista
+print(len(frutas))
+
+#Quero incluir uma fruta nova
+frutas.append("manga")
+
+print(len(frutas)) #lengh = tamanho
+print(frutas)
+
+print(frutas[0])
+print(frutas[1])
+print(frutas[2])
+print(frutas[3])
+
+i=0
+while(i<4):
+  print(frutas[i])
+  i = i + 1
+
+ 
+print("Exemplo das frutas com FOR")
+for fruta in frutas:
+  print(frutas)
