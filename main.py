c3dedaa92ccaf0aff6dd2b206c818974e8d6cd66
Diogo Lacerda <116228981+Diogolcs@users.noreply.github.com>
2022-11-17 01:03:15 +0000

    a

--- main.py
+++ main.py
@@ -189,20 +189,77 @@
 #for pessoa in pessoas:
 #  print(f"Nome:{pessoa[1]} ({pessoa[3]})")
 
-print("--------------------------------------------------------\n")
+
+
 #1o. passo: importar a biblioteca sqlite3
-import sqlite3
+# import sqlite3
 
 #2o. passo: Vamos estabelecer uma 
 #conexão com o banco de dados
-conexao = sqlite3.connect("dc_universe.db")
+# conexao = sqlite3.connect("dc_universe.db")
 
 #3o. passo: criar um objeto do tipo cursor
-cursor = conexao.cursor()
+# cursor = conexao.cursor()
 
 #4o. passo: comando para inserir um herói/vilão
-sql = "INSERT INTO pessoas (pessoa_id, nome, nome_civil, tipo) VALUES (12, 'The Flash', 'Barry Allen', 'Herói(na)')"
+# sql = "INSERT INTO pessoas (pessoa_id, nome, nome_civil, tipo) VALUES (12, 'The Flash', 'Barry Allen', 'Herói(na)')"
 
-#5o passo: EXecutar o comando SQL
-print(cursor.execute(sql))
+#5o. passo: Executar o comando SQL
+# cursor.execute(sql)
+
+#6o. passo: Confirmar o INSERT com commit() e fechar o banco
+# conexao.commit()
+# conexao.close()
+
+#1o. passo: importar a biblioteca sqlite3
+#import sqlite3
+
+
+#PASSOS 2 e 3 VIRARÃO função conectar()
+#def conectar():
+#2o. passo: Vamos estabelecer uma
+#conexão com o banco de dados
+# conexao = sqlite3.connect("dc_universe.db")
+
+#3o. passo: criar um objeto do tipo cursor
+#cursor = conexao.cursor()
+
+#return conexao, cursor
+
+#import sqlite3 as bd
+
+#con, cur = bd.conectar()
+
+#nome = input("Informe o nome do herói/vilão: ")
+#nome_civil = input("Informe o nome civil do herói/vilão (sua identidade secreta): ")
+#tipo_numerico = input("Tecle 1 para Herói(na) ou 2 para Vilã(o)")
+
+#Consulta para o valor máximo usado no banco
+#sql = "SELECT MAX(pessoa_id)+1 FROM pessoas"
+#cur.execute(sql)
+#numero = cur.fetchone()[0]
+#numero
+
+
+import sqlite3 as bd
+
+
+
+con, cur = bd.conectar()
+
+nome = input("Informe o nome do herói/vilão: ")
+nome_civil = input("Informe o nome civil do herói/vilão (sua identidade secreta): ")
+tipo_numerico = input("Tecle 1 para Herói(na) ou 2 para Vilã(o)")
+
+#Consulta para o valor máximo usado no banco
+sql = "SELECT MAX(pessoa_id)+1 FROM pessoas"
+cur.execute(sql)
+pessoa_id = cur.fetchone()[0]
+
+if tipo_numerico == "1":
+  tipo = "Herói(na)"
+else:
+  tipo = "Vilã(o)"
+
+sql = f"INSERT INTO pessoas (pessoa_id, nome, nome_civil, tipo) VALUES ({pessoa_id}, '{nome}', '{nome_civil}', '{tipo}')"
 
